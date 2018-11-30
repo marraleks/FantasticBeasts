a4ae9f11ed4690bf7d917ae43a68403b9437d9f1
Marius Aleksander Sletten <MariusAleksanderSletten@Mariuss-MacBook-Pro.local>
2018-11-30 09:25:33 +0100

    Added fabeldyr

---
+++ fabeldyr/js/feet.js
@@ -0,0 +1,321 @@
+$('#f1').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 600 && $(this).scrollTop() < 700) {
+    $('#f1').fadeIn();
+  } else {
+    $('#f1').fadeOut();
+  }
+});  
+
+$('#f1r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 700 && $(this).scrollTop() < 800) {
+    $('#f1r').fadeIn();
+  } else {
+    $('#f1r').fadeOut();
+  }
+});  
+
+$('#f2').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 800 && $(this).scrollTop() < 900) {
+    $('#f2').fadeIn();
+  } else {
+    $('#f2').fadeOut();
+  }
+});  
+
+$('#f2r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 900 && $(this).scrollTop() < 1000) {
+    $('#f2r').fadeIn();
+  } else {
+    $('#f2r').fadeOut();
+  }
+});
+
+$('#f3').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1000 && $(this).scrollTop() < 1100) {
+    $('#f3').fadeIn();
+  } else {
+    $('#f3').fadeOut();
+  }
+});  
+
+$('#f3r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1100 && $(this).scrollTop() < 1200) {
+    $('#f3r').fadeIn();
+  } else {
+    $('#f3r').fadeOut();
+  }
+});  
+
+$('#f4').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1200 && $(this).scrollTop() < 1300) {
+    $('#f4').fadeIn();
+  } else {
+    $('#f4').fadeOut();
+  }
+});  
+
+$('#f4r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1300 && $(this).scrollTop() < 1400) {
+    $('#f4r').fadeIn();
+  } else {
+    $('#f4r').fadeOut();
+  }
+});  
+
+$('#f5').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1700 && $(this).scrollTop() < 1800) {
+    $('#f5').fadeIn();
+  } else {
+    $('#f5').fadeOut();
+  }
+});  
+
+$('#f5r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1800 && $(this).scrollTop() < 1900) {
+    $('#f5r').fadeIn();
+  } else {
+    $('#f5r').fadeOut();
+  }
+});  
+
+$('#f6').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 1900 && $(this).scrollTop() < 2000) {
+    $('#f6').fadeIn();
+  } else {
+    $('#f6').fadeOut();
+  }
+});  
+
+$('#f6r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2000 && $(this).scrollTop() < 2100) {
+    $('#f6r').fadeIn();
+  } else {
+    $('#f6r').fadeOut();
+  }
+});  
+
+$('#f7').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2100 && $(this).scrollTop() < 2200) {
+    $('#f7').fadeIn();
+  } else {
+    $('#f7').fadeOut();
+  }
+});  
+
+$('#f7r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2200 && $(this).scrollTop() < 2300) {
+    $('#f7r').fadeIn();
+  } else {
+    $('#f7r').fadeOut();
+  }
+});  
+
+$('#f8').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2300 && $(this).scrollTop() < 2400) {
+    $('#f8').fadeIn();
+  } else {
+    $('#f8').fadeOut();
+  }
+});  
+
+$('#f8r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2400 && $(this).scrollTop() < 2500) {
+    $('#f8r').fadeIn();
+  } else {
+    $('#f8r').fadeOut();
+  }
+});  
+
+$('#f9').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2600 && $(this).scrollTop() < 2700) {
+    $('#f9').fadeIn();
+  } else {
+    $('#f9').fadeOut();
+  }
+});  
+
+$('#f9r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2700 && $(this).scrollTop() < 2800) {
+    $('#f9r').fadeIn();
+  } else {
+    $('#f9r').fadeOut();
+  }
+});
+
+$('#f10').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2800 && $(this).scrollTop() < 2900) {
+    $('#f10').fadeIn();
+  } else {
+    $('#f10').fadeOut();
+  }
+});  
+
+$('#f10r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 2900 && $(this).scrollTop() < 3000) {
+    $('#f10r').fadeIn();
+  } else {
+    $('#f10r').fadeOut();
+  }
+});  
+
+$('#f11').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3000 && $(this).scrollTop() < 3100) {
+    $('#f11').fadeIn();
+  } else {
+    $('#f11').fadeOut();
+  }
+});  
+
+$('#f11r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3100 && $(this).scrollTop() < 3200) {
+    $('#f11r').fadeIn();
+  } else {
+    $('#f11r').fadeOut();
+  }
+});  
+
+$('#f12').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3200 && $(this).scrollTop() < 3300) {
+    $('#f12').fadeIn();
+  } else {
+    $('#f12').fadeOut();
+  }
+});  
+
+$('#f12r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3300 && $(this).scrollTop() < 3400) {
+    $('#f12r').fadeIn();
+  } else {
+    $('#f12r').fadeOut();
+  }
+});  
+
+$('#f13').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3400 && $(this).scrollTop() < 3500) {
+    $('#f13').fadeIn();
+  } else {
+    $('#f13').fadeOut();
+  }
+});  
+
+$('#f13r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3500 && $(this).scrollTop() < 3600) {
+    $('#f13r').fadeIn();
+  } else {
+    $('#f13r').fadeOut();
+  }
+});  
+
+$('#f14').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3600 && $(this).scrollTop() < 3700) {
+    $('#f14').fadeIn();
+  } else {
+    $('#f14').fadeOut();
+  }
+});  
+
+$('#f14r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3700 && $(this).scrollTop() < 3800) {
+    $('#f14r').fadeIn();
+  } else {
+    $('#f14r').fadeOut();
+  }
+});  
+
+$('#f15').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3800 && $(this).scrollTop() < 3900) {
+    $('#f15').fadeIn();
+  } else {
+    $('#f15').fadeOut();
+  }
+});  
+
+$('#f15r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3900 && $(this).scrollTop() < 3910) {
+    $('#f15r').fadeIn();
+  } else {
+    $('#f15r').fadeOut();
+  }
+});  
+
+$('#f16').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3910 && $(this).scrollTop() < 4000) {
+    $('#f16').fadeIn();
+  } else {
+    $('#f16').fadeOut();
+  }
+});  
+
+$('#f16r').hide();
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() > 3920 && $(this).scrollTop() < 4000) {
+    $('#f16r').fadeIn();
+  } else {
+    $('#f16r').fadeOut();
+  }
+});  
+
+
